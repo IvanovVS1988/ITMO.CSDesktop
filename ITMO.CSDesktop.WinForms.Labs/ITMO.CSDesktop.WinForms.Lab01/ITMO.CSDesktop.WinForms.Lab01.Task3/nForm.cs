9de5db4b9cc0180a2e6df5bbb2ba2db245da05e4
Владимир Иванов <ivanovladimir1988@gmail.com>
2022-07-04 00:47:28 +0300

    Add project files.

---
+++ ITMO.CSDesktop.WinForms.Labs/ITMO.CSDesktop.WinForms.Lab01/ITMO.CSDesktop.WinForms.Lab01.Task3/nForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ITMO.CSDesktop.WinForms.Lab01.Task3
+{
+    public partial class nForm : ITMO.CSDesktop.WinForms.Lab01.Task3.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
