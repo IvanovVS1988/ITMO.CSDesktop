623e4100727e883f036a1e49b495315d0d761330
Владимир Иванов <ivanovladimir1988@gmail.com>
2022-07-10 15:21:41 +0300

    Lab05 finished

---
+++ ITMO.CSDesktop.WinForms.Labs/ITMO.CSDesktop.WinForms.Lab05/ITMO.CSDesktop.WinForms.Lab05.Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using ExcelObj = Microsoft.Office.Interop.Excel;
+
+namespace ITMO.CSDesktop.WinForms.Lab05.Task1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
