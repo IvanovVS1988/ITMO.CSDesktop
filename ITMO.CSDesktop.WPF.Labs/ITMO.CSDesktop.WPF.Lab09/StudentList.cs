c28d0eafaf9c7fa7ed7b70e1bc12fda61fc66a39
Владимир Иванов <ivanovladimir1988@gmail.com>
2022-07-15 22:56:28 +0300

    WPF Lab09 finished

---
+++ ITMO.CSDesktop.WPF.Labs/ITMO.CSDesktop.WPF.Lab09/StudentList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+
+namespace ITMO.CSDesktop.WPF.Lab09.Task1
+{
+    public class StudentList: ObservableCollection<Student>
+    {
+    }
+}
